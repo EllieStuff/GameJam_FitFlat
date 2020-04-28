17f184d55002fb9ece17f01492153dffb0d2d426
CescBalsells <cescbalsells8@gmail.com>
2020-04-28 12:18:33 +0200

    Menu inicial provisional i assets menu afegits

---
+++ Game_Jam_Project/Assets/Script/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    Button playBtn;
+    Button optionsBtn;
+    Button exitBtn;
+
+
+    void Start()
+    {
+        playBtn = GameObject.Find("PlayButton").GetComponent<Button>();
+        optionsBtn = GameObject.Find("OptionsButton").GetComponent<Button>();
+        // exitBtn = GameObject.Find("ExitButton").GetComponent<Button>();
+
+        playBtn.onClick.AddListener(()=>LoadScene("Gameplay"));
+        // exitBtn.onClick.AddListener(() => ExitGame());
+    }
+
+    void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
