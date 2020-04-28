d7a59340f48e7efe48508fa044d7db1a221cf13a
Martí <marti.deferrer@enti.cat>
2020-04-28 13:11:39 +0200

    Error

--- Game_Jam_Project/Assets/Script/ExercisesClass.cs
+++ Game_Jam_Project/Assets/Script/ExercisesClass.cs
@@ -14,42 +14,42 @@ public class ExercisesClass : MonoBehaviour
     public bool isCompleted;
     public float exerciseCombo;
 
-    private void Start()
-    {
-//<<<<<<< Updated upstream
-//        ////Sentadillas
-//        //exercises[(int)Const.Difficulty.LOW, (int)Const.Quantity.LOW, (int)Const.Exercise.SENTADILLAS] = 10;
-//        //exercises[(int)Const.Difficulty.LOW, (int)Const.Quantity.HIGH, (int)Const.Exercise.SENTADILLAS] = 30;
+//    private void Start()
+//    {
+////<<<<<<< Updated upstream
+////        ////Sentadillas
+////        //exercises[(int)Const.Difficulty.LOW, (int)Const.Quantity.LOW, (int)Const.Exercise.SENTADILLAS] = 10;
+////        //exercises[(int)Const.Difficulty.LOW, (int)Const.Quantity.HIGH, (int)Const.Exercise.SENTADILLAS] = 30;
 
-//        //exercises[(int)Const.Difficulty.MEDIUM, (int)Const.Quantity.LOW, (int)Const.Exercise.SENTADILLAS] = 15;
-//        //exercises[(int)Const.Difficulty.MEDIUM, (int)Const.Quantity.HIGH, (int)Const.Exercise.SENTADILLAS] = 35;
+////        //exercises[(int)Const.Difficulty.MEDIUM, (int)Const.Quantity.LOW, (int)Const.Exercise.SENTADILLAS] = 15;
+////        //exercises[(int)Const.Difficulty.MEDIUM, (int)Const.Quantity.HIGH, (int)Const.Exercise.SENTADILLAS] = 35;
 
-//        //exercises[(int)Const.Difficulty.HIGH, (int)Const.Quantity.LOW, (int)Const.Exercise.SENTADILLAS] = 20;
-//        //exercises[(int)Const.Difficulty.HIGH, (int)Const.Quantity.HIGH, (int)Const.Exercise.SENTADILLAS] = 40;
-
-
-//        ////Flexiones
-//        //exercises[(int)Const.Difficulty.LOW, (int)Const.Quantity.LOW, (int)Const.Exercise.FLEXIONES] = 5;
-//        //exercises[(int)Const.Difficulty.LOW, (int)Const.Quantity.HIGH, (int)Const.Exercise.FLEXIONES] = 20;
-
-//        //exercises[(int)Const.Difficulty.MEDIUM, (int)Const.Quantity.LOW, (int)Const.Exercise.FLEXIONES] = 10;
-//        //exercises[(int)Const.Difficulty.MEDIUM, (int)Const.Quantity.HIGH, (int)Const.Exercise.FLEXIONES] = 30;
-
-//        //exercises[(int)Const.Difficulty.HIGH, (int)Const.Quantity.LOW, (int)Const.Exercise.FLEXIONES] = 20;
-//        //exercises[(int)Const.Difficulty.HIGH, (int)Const.Quantity.HIGH, (int)Const.Exercise.FLEXIONES] = 40;
+////        //exercises[(int)Const.Difficulty.HIGH, (int)Const.Quantity.LOW, (int)Const.Exercise.SENTADILLAS] = 20;
+////        //exercises[(int)Const.Difficulty.HIGH, (int)Const.Quantity.HIGH, (int)Const.Exercise.SENTADILLAS] = 40;
 
 
-//        ////Etc.
+////        ////Flexiones
+////        //exercises[(int)Const.Difficulty.LOW, (int)Const.Quantity.LOW, (int)Const.Exercise.FLEXIONES] = 5;
+////        //exercises[(int)Const.Difficulty.LOW, (int)Const.Quantity.HIGH, (int)Const.Exercise.FLEXIONES] = 20;
+
+////        //exercises[(int)Const.Difficulty.MEDIUM, (int)Const.Quantity.LOW, (int)Const.Exercise.FLEXIONES] = 10;
+////        //exercises[(int)Const.Difficulty.MEDIUM, (int)Const.Quantity.HIGH, (int)Const.Exercise.FLEXIONES] = 30;
+
+////        //exercises[(int)Const.Difficulty.HIGH, (int)Const.Quantity.LOW, (int)Const.Exercise.FLEXIONES] = 20;
+////        //exercises[(int)Const.Difficulty.HIGH, (int)Const.Quantity.HIGH, (int)Const.Exercise.FLEXIONES] = 40;
+
+
+////        ////Etc.
         
 
 
-//=======
+////=======
         
-//>>>>>>> Stashed changes
+////>>>>>>> Stashed changes
+////    }
+
+//    private void Update()
+//    {
+        
 //    }
-
-    private void Update()
-    {
-        
-    }
-}
+//}
