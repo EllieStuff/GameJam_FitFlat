b3c288f2b4ec9c7c02619576a725f57f6dd230c0
Michael Miranda <angel.miranda@enti.cat>
2020-04-28 16:02:55 +0200

    commit

--- Game_Jam_Project/Assets/Script/Player.cs
+++ Game_Jam_Project/Assets/Script/Player.cs
@@ -1,28 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
 using UnityEngine;
 
-public class UIManager : MonoBehaviour
+public class Player : MonoBehaviour
 {
-
-    Button man_button;
-    Button girl_button;
-
     // Start is called before the first frame update
     void Start()
     {
-        man_button.onClick.AddListener(() => SelectGenere());
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    void SelectGenere()
-    {
-
     }
 }
