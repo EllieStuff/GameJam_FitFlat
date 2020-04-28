6454096ee0b368b5d590498520727ec1f606ace4
CescBalsells <cescbalsells8@gmail.com>
2020-04-28 20:11:07 +0200

    AudioImplementat

--- Game_Jam_Project/Assets/Script/AudioManager.cs
+++ Game_Jam_Project/Assets/Script/AudioManager.cs
@@ -2,49 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
+    [SerializeField] AudioMixer audioMixer;
+
     [SerializeField] AudioClip[] musicClips;
     [SerializeField] AudioClip[] effectClips;
+
     [SerializeField] AudioMixerSnapshot paused;
     [SerializeField] AudioMixerSnapshot unPaused;
+
+    [SerializeField] Slider effectsSlider;
+    [SerializeField] Slider musicSlider;
+
     AudioSource backgroundMusic;
     AudioSource soundEffects;
 
-    //private void Start()
-    //{
-    //    AudioSource[] audio = GetComponentsInChildren<AudioSource>();
-    //    backgroundMusic = audio[0];
-    //    soundEffects = audio[1];
+    private void Awake()
+    {
+        AudioSource[] audio = GetComponentsInChildren<AudioSource>();
+        backgroundMusic = audio[0];
+        soundEffects = audio[1];
 
-    //}
 
-    //void Update()
-    //{
-    //    if(!backgroundMusic.isPlaying)
-    //    {
-    //        backgroundMusic.clip = GetRandomClip();
-    //        backgroundMusic.Play();
-    //    }
-    //}
 
-    //AudioClip GetRandomClip()
-    //{
-    //    return musicClips[Random.Range(0, musicClips.Length)];
-    //}
+        // Llegim el valor del volum guardat i l'actualitzem
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 0);
+        SetMusicVolume(musicSlider.value);
+        effectsSlider.value = PlayerPrefs.GetFloat("EffectsVolume", 0);
+        SetEffectsVolume(effectsSlider.value);
 
-    //public void PauseAudio(bool active)
-    //{
-    //    if (active)
-    //        paused.TransitionTo(0.01f);
-    //    else
-    //        unPaused.TransitionTo(0.01f);
+        Button[] buttons = FindObjectsOfType<Button>();
+        foreach(Button b  in buttons)
+        {
+            b.onClick.AddListener(() => PlayClickButton());
+        }
+    }
 
-    //}
+    void Update()
+    {
+        if (!backgroundMusic.isPlaying)
+        {
+            backgroundMusic.clip = GetRandomClip();
+            backgroundMusic.Play();
+        }
+    }
 
-    //public void PlayClickButton()
-    //{
-    //    soundEffects.PlayOneShot(effectClips[0]);
-    //}
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+        PlayerPrefs.SetFloat("MusicVolume", volume);
+    }
+
+    public void SetEffectsVolume(float volume)
+    {
+        audioMixer.SetFloat("EffectsVolume", volume);
+        PlayerPrefs.SetFloat("EffectsVolume", volume);
+    }
+
+    AudioClip GetRandomClip()
+    {
+        return musicClips[Random.Range(0, musicClips.Length)];
+    }
+
+    public void PauseAudio(bool active)
+    {
+        if (active)
+            paused.TransitionTo(0.01f);
+        else
+            unPaused.TransitionTo(0.01f);
+
+    }
+
+    public void PlayClickButton()
+    {
+        soundEffects.PlayOneShot(effectClips[0]);
+    }
 }
--- Game_Jam_Project/Assets/Script/GameManager.cs
+++ Game_Jam_Project/Assets/Script/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject.Find("ChooseLevel").SetActive(false);
     }
 
     // Update is called once per frame
